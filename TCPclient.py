df38867a876e6e49063b2829e7ae966afa0583be
smftn <ivisivisiv@gmail.com>
2019-11-10 20:15:25 +0000

    comments

--- TCPclient.py
+++ TCPclient.py
@@ -2,7 +2,12 @@ import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket object
 host = socket.gethostname()
-port = 9999
-clientsocket.connect((host, port))
-message = clientsocket.recv(1024)
-print(message.decode("utf-8"))
+port = 2562
+clientsocket.connect((host, port)) #connecting to the server
+complete_info = '' #complete information to be recieved, variable with an empty string
+while True:
+        message = clientsocket.recv(10)
+        if len (message)<=0:
+            break
+        complete_info += (message.decode("utf-8")) #decoding the message in utf8 bytes
+print(complete_info)
--- TCPserver.py
+++ TCPserver.py
@@ -1,13 +1,13 @@
 import socket
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating socket object, SOCK_STREAM is used to create tcp protocols
 host = socket.gethostname()
-port = 9999
-serversocket.bind((host,port))
+port = 2562
+serversocket.bind((host,port)) #binding to the client, used when server and client are on the same device
 serversocket.listen(5)
 
-while True:
+while True: #accept the client socket and address
     clt, adr=serversocket.accept()
     print(f"connection to {adr} successful")
-    clt.send(bytes("this is a tcp socket program in python", "utf-8"))
-
+    clt.send(bytes("this is a tcp socket program in python", "utf-8")) #message to client in utf8 bytes
+    clt.close() #close communication
